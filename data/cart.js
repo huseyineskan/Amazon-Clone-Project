7c683c4364cd854617be380abc57d82317343613
Hüseyin ESKAN <huseyineskan01@gmail.com>
2023-09-04 22:30:43 +0200

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,21 @@
 export const cart = [];
+
+export function addToCart(productId) {
+    const productQuantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+    let matchingItem;
+
+    cart.forEach((item) => {
+        if(productId === item.productId) {
+            matchingItem = item;
+        }
+    });
+
+    if(matchingItem) {
+        matchingItem.quantity += productQuantity;
+    } else {
+        cart.push({
+            productId: productId,
+            quantity: productQuantity
+        });
+    }
+}
